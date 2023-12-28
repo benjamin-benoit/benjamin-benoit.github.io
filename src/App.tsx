c10a0bff62cc858387ab7f51529f0378d795c4ed
Benjamin <benjaminbenoitfr@gmail.com>
2023-12-28 14:28:36 +0100

    :sparkles: update website

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
 import "./App.css";
 import Footer from "./Components/Footer";
 import Me from "./Components/Me";
-import Particles from "./Components/Particles";
 import Presentation from "./Components/Presentation";
-import Story from "./Components/Story";
 
 const App = () => {
   return (
-    <>
-      <Particles />
-
-      <div className="container">
-        <div className="section">
-          <div className="bloc">
-            <Me />
-            <Presentation />
-          </div>
-        </div>
-        <div className="section">
-          <span className="wave" role="img" aria-labelledby="wave">
-            💼
-          </span>
-          <h4>Work in Paris</h4>
-        </div>
-        <div className="section">
-          <Story />
-        </div>
-        <div className="section">
-          <Footer />
+    <div className="container">
+      <div className="section">
+        <img className="illu" src="../src/assets/benjy_ben_send-01.png" />
+        <div className="bloc">
+          <Me />
+          <Presentation />
         </div>
       </div>
-    </>
+      <Footer />
+    </div>
   );
 };
 
--- src/Components/Me.tsx
+++ src/Components/Me.tsx
@@ -1,3 +1,10 @@
 export default function Me() {
-  return <h1 className="me">Hey, I'm Benjamin !</h1>;
+  return (
+    <h1 className="me">
+      Hey, I'm Benjamin !
+      <span className="wave" role="img" aria-labelledby="wave">
+        👋
+      </span>
+    </h1>
+  );
 }
